98b1725871adc29e1c36839c415d5b1e15d1ba0d
rladydqls99 <rladydqls99@naver.com>
2025-11-27 18:54:44 +0900

    Add Domain model to Sequelize initialization and associations

--- models/index.js
+++ models/index.js
@@ -6,6 +6,7 @@ const config = require("../config/config")[env];
 const User = require("./user");
 const Post = require("./post");
 const Hashtag = require("./hashtag");
+const Domain = require("./domain");
 
 const db = {};
 
@@ -20,14 +21,17 @@ db.sequelize = sequelize;
 db.User = User;
 db.Post = Post;
 db.Hashtag = Hashtag;
+db.Domain = Domain;
 
 User.initiate(sequelize);
 Post.initiate(sequelize);
 Hashtag.initiate(sequelize);
+Domain.initiate(sequelize);
 
 User.associate(db);
 Post.associate(db);
 Hashtag.associate(db);
+Domain.associate(db);
 
 module.exports = db;
 
